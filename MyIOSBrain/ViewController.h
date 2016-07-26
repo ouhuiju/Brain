857ce104f7e31c5a5127181c293b2ab19dd9c43c
OKAR OU <ouq68@qq.com>
2016-07-26 21:28:46 +0800

    project init.

---
+++ MyIOSBrain/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyIOSBrain
+//
+//  Created by 区慧驹 on 16/7/26.
+//  Copyright © 2016年 OkarOu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController<UITableViewDelegate, UITableViewDataSource>
+
+
+@end
+
---
+++ MyIOSBrain/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyIOSBrain
+//
+//  Created by 区慧驹 on 16/7/26.
+//  Copyright © 2016年 OkarOu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"MyIOSBrain";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
