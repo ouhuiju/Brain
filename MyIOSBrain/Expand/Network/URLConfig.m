8cb9d9378ac88eae7e4398b43ff7fe3bdc774655
OKAR OU <okar.ou@oocl.com>
2016-07-28 16:10:02 +0800

    add URLConfig

---
+++ MyIOSBrain/Expand/Network/URLConfig.m
@@ -0,0 +1,13 @@
+//
+//  URLConfig.m
+//  MyIOSBrain
+//
+//  Created by OKAR OU on 16/7/28.
+//  Copyright © 2016年 OkarOu. All rights reserved.
+//
+
+#import "URLConfig.h"
+
+@implementation URLConfig
+
+@end
