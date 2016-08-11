b2b90d70de552982a616cd219fbd388d70bc13da
okar <okar.ou@oocl.com>
2016-08-11 17:20:50 +0800

    add web view module.

---
+++ MyIOSBrain/Main/HybridView/Controller/HybridViewController.h
@@ -0,0 +1,16 @@
+//
+//  HybridViewController.h
+//  MyIOSBrain
+//
+//  Created by OKAR OU on 16/8/11.
+//  Copyright © 2016年 OkarOu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <WebViewJavascriptBridge/WebViewJavascriptBridge.h>
+
+@interface HybridViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIWebView *hybridWebView;
+
+@end
