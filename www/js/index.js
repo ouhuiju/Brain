c26c076818e38bf5664c32d88a0644b791048390
OKAR OU <okar.ou@oocl.com>
2016-08-17 16:51:41 +0800

    update hybrid module.

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,37 @@
-var mainViewModel = new Vue({
-	el: '#app',
-	data: {
-		message: 'Hello Vue.js!'
-	}
+F7.init();
+
+$(document).ready(function () {
+	console.log('document ready');
+	indexModule.initPage();
 });
+
+var indexViewModel = new Vue({
+	el: '#indexViewModel',
+	data: {
+		todos: [
+			{
+				text: 'Learn JavaScript'
+			},
+			{
+				text: 'Learn Vue.js'
+			},
+			{
+				text: 'Build Something Awesome'
+			}
+    ]
+	}
+})
+
+var indexModule = (function () {
+	var param = {
+
+	};
+
+	var initPage = function () {
+
+	};
+
+	return {
+		initPage: initPage
+	};
+})();
